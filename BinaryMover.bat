f379595a84740cf768c1fd446ae18aa8c3e60289
coding-with-nilesh <95519505+coding-with-nilesh@users.noreply.github.com>
2021-12-17 11:11:18 +0530

    Added solution

---
+++ BinaryMover.bat
@@ -0,0 +1,11 @@
+xcopy /s One.Core\bin\Debug\net5.0\One.Core.dll ..\References\One /y
+xcopy /s One.Core\bin\Debug\net5.0\One.Core.pdb ..\References\One /y
+
+xcopy /s One.Helper\bin\Debug\net5.0\One.Helper.dll ..\References\One /y
+xcopy /s One.Helper\bin\Debug\net5.0\One.Helper.pdb ..\References\One /y
+
+xcopy /s One.Infrastructure\bin\Debug\net5.0\One.Infrastructure.dll ..\References\One /y
+xcopy /s One.Infrastructure\bin\Debug\net5.0\One.Infrastructure.pdb ..\References\One /y
+
+
+
---
+++ ProjectCleaner.bat
@@ -0,0 +1,8 @@
+rmdir One.Core\obj /s /q
+rmdir One.Core\bin /s /q
+rmdir One.Helper\obj /s /q
+rmdir One.Helper\bin /s /q
+rmdir One.Infrastructure\obj /s /q
+rmdir One.Infrastructure\bin /s /q
+rmdir .vs /s /q
+
