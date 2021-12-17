a8ee2bea8f4502a0a293b3cd3271ab44ef486b47
coding-with-nilesh <95519505+coding-with-nilesh@users.noreply.github.com>
2021-12-17 11:11:53 +0530

    Added initializer

---
+++ One.Core/Initialization/IInitializable.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+
+namespace One.Core.Initialization
+{
+    /// <summary>
+    /// Interface for objects that can be initialized
+    /// </summary>
+    public interface IInitializable
+    {
+        /// <summary>
+        /// Specifies whether the instance has been initialized successfully
+        /// </summary>
+        bool IsInitialized { get; }
+
+        /// <summary>
+        /// Initializes the task.
+        /// </summary>
+        /// <returns></returns>
+        Task InitializeAsync();
+
+        /// <summary>
+        /// Waits for initialization task to complete
+        /// </summary>
+        /// <returns></returns>
+        Task AwaitInitialization();
+    }
+}
---
+++ One.Core/Initialization/IInitializer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace One.Core.Initialization
+{
+    public interface IInitializer: IInitializable
+    {
+        void SetInitializationWork(Func<Task> initializationWork);
+    }
+}
