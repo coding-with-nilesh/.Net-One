095a3d3c5dd5a21ddd6863f7d81d7ee44e55ec81
coding-with-nilesh <95519505+coding-with-nilesh@users.noreply.github.com>
2021-12-17 11:13:09 +0530

    Initializer changes

---
+++ One.Infrastructure/Initialization/Initializable.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using One.Core.Initialization;
+
+namespace One.Infrastructure.Initialization
+{
+    public class Initializable : IInitializable
+    {
+        #region Declarations
+        private Task _completedTask = Task.Delay(0);
+        private Initializer _initializer;
+        #endregion
+
+        #region Ctor
+        public Initializable()
+        {
+            _initializer = new Initializer(Initialize);
+        }
+        #endregion
+
+        #region Properties
+        public bool IsInitialized { get { return _initializer.IsInitialized; } }
+        public Task CompletedTask => _completedTask;
+        #endregion
+
+        #region Public methods
+        public virtual Task InitializeAsync()
+        {
+            return _initializer.InitializeAsync();
+        }
+
+        public Task AwaitInitialization()
+        {
+            return _initializer.AwaitInitialization();
+        }
+        #endregion
+
+        #region Helpers
+
+        // Actually performs the initialization
+        protected virtual Task Initialize()
+        {
+            return CompletedTask;
+        }
+        #endregion
+    }
+}
+
+
---
+++ One.Infrastructure/Initialization/Initializer.cs
@@ -0,0 +1,73 @@
+﻿using System.Threading.Tasks;
+using System;
+using One.Core.Initialization;
+
+namespace One.Infrastructure.Initialization
+{
+    public class Initializer : IInitializable
+    {
+        #region Declarations
+        protected readonly object SyncObject = new object();
+        protected Task InitilizationTask;
+        private Func<Task> _initializationWork;
+        #endregion
+
+        #region Ctor
+        public Initializer()
+        {
+        }
+
+        public Initializer(Func<Task> initializationWork)
+        {
+            SetInitializationWork(initializationWork);
+        }
+
+        #endregion
+
+        #region Properties
+        public bool IsInitialized { get; protected set; }
+        #endregion
+
+        #region Public methods
+        public void SetInitializationWork(Func<Task> initializationWork)
+        {
+            lock (SyncObject)
+            {
+                if (_initializationWork != null)
+                {
+                    throw new NotSupportedException("Initialization Work can be set only once.");
+                }
+
+                _initializationWork = initializationWork;
+            }
+        }
+
+        public Task InitializeAsync()
+        {
+            // start the task if its not already started
+            lock (SyncObject)
+            {
+                if (_initializationWork == null)
+                {
+                    throw new ArgumentNullException("InitializationWork can't be null");
+                }
+
+                if (InitilizationTask == null)
+                {
+                    InitilizationTask = _initializationWork();
+                    InitilizationTask.ContinueWith(task => { IsInitialized = true; }, TaskContinuationOptions.OnlyOnRanToCompletion);
+                }
+            }
+
+            return InitilizationTask;
+        }
+
+        public async Task AwaitInitialization()
+        {
+            await InitializeAsync().ConfigureAwait(false);
+        }
+        #endregion
+    }
+}
+
+
