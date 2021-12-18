9e93e769cb00005a04bf4346a1751ddd475610a2
coding-with-nilesh <95519505+coding-with-nilesh@users.noreply.github.com>
2021-12-18 14:54:48 +0530

    Added factory and GUID

---
+++ One.Core/DI/IFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace One.Core.DI
+{
+    public interface IFactory
+    {
+        void RegisterSingleton<TBase, TImplementation>()
+            where TImplementation : TBase;
+
+        void RegisterScoped<TBase, TImplementation>()
+            where TImplementation : TBase;
+
+        T Resolve<T>();
+    }
+}
---
+++ One.Infrastructure/Guid/GuidService.cs
@@ -0,0 +1,18 @@
+﻿using One.Core.Guid;
+using System;
+using GUID = System.Guid;
+
+namespace One.Infrastructure.Guid
+{
+    public class GuidService : IGuidService
+    {
+        public string GetGuid() => GUID.NewGuid().ToString();
+
+        public string GetShortGuid()
+        {
+            var ticks = new DateTime(2021, 1, 1).Ticks; // First data which will be used as reference. Keep it latest to reduce length of id.
+            var ans = DateTime.Now.Ticks - ticks;
+            return ans.ToString("x");
+        }
+    }
+}
