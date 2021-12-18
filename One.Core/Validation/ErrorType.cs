fa84f8b794d2d3a7e544dd202c2ddb1c230cb060
coding-with-nilesh <95519505+coding-with-nilesh@users.noreply.github.com>
2021-12-18 19:53:37 +0530

    Update ErrorType.cs

--- One.Core/Validation/ErrorType.cs
+++ One.Core/Validation/ErrorType.cs
@@ -1,11 +1,11 @@
 ﻿namespace One.Core.Validation
 {
-    public enum ErrorType
+    public class ErrorType
     {
-        Missing,
-        MissingOrIncorrect,
-        Duplicate,
-        DataTypeMismatch,
-        OutOfRange
+        public const string Missing = "Missing";
+        public const string MissingOrIncorrect = "MissingOrIncorrect";
+        public const string Duplicate = "Duplicate";
+        public const string DataTypeMismatch = "DataTypeMismatch";
+        public const string OutOfRange = "OutOfRange";
     }
 }
