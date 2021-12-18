2b45db09806d600b9393dd37a7b84bb111d1a5ae
coding-with-nilesh <95519505+coding-with-nilesh@users.noreply.github.com>
2021-12-18 14:55:55 +0530

    Create SerializationService.cs

---
+++ One.Infrastructure/Serialization/SerializationService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using One.Core.Serialization;
+
+namespace One.Infrastructure.Serialization
+{
+    public class SerializationService : ISerializationService
+    {
+        public Task SerializeAsync(object obj, string outputPath)
+        {
+            return Task.Run(async () =>
+             {
+                 StreamWriter writer = null;
+
+                 try
+                 {
+                     writer = new StreamWriter(outputPath);
+                     await SerializeAsync(obj, writer).ConfigureAwait(false);
+                 }
+                 finally
+                 {
+                     writer?.Close();
+                 }
+             });
+        }
+
+        public Task SerializeAsync(object obj, StreamWriter streamWriter)
+        {
+            return Task.Run(() =>
+            {
+                var serializer = new XmlSerializer(obj.GetType());
+                serializer.Serialize(streamWriter, obj);
+            });
+        }
+
+        public Task<object> DeserializeAsync(string inputPath, Type type)
+        {
+            return Task.Run(async () =>
+            {
+                StreamReader streamReader = null;
+
+                try
+                {
+                    var serializer = new XmlSerializer(type);
+                    return await DeserializeAsync(streamReader, type).ConfigureAwait(false);
+                }
+                finally
+                {
+                    streamReader?.Close();
+                }
+            });
+        }
+
+        public Task<object> DeserializeAsync(StreamReader streamReader, Type type)
+        {
+            return Task.Run(() =>
+            {
+                var serializer = new XmlSerializer(type);
+                return serializer.Deserialize(streamReader);
+            });
+        }
+    }
+}
