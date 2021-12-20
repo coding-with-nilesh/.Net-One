3eaac648e2d3d00eb33eb50db684cbc8178b3d7a
coding-with-nilesh <95519505+coding-with-nilesh@users.noreply.github.com>
2021-12-20 16:35:13 +0530

    Create JsonHelper.cs

---
+++ One.Core/Json/JsonHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace One.Core.Json
+{
+    public class JsonHelper
+    {
+        public static T Deserialize<T>(string value)
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            options.Converters.Add(new JsonStringEnumConverter());
+
+            return JsonSerializer.Deserialize<T>(value, options);
+        }
+    }
+}
