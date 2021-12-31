2ce62daf42e7fefaa326dd24c47609ce61c1efbc
coding-with-nilesh <95519505+coding-with-nilesh@users.noreply.github.com>
2021-12-31 23:17:08 +0530

    Update UserBase.cs

--- One.Core/Models/Authentication/UserBase.cs
+++ One.Core/Models/Authentication/UserBase.cs
@@ -12,6 +12,8 @@ namespace One.Core.Models.Authentication
     public abstract class UserBase
     {
         public string Id { get; set; }
+        public string EmailId { get; set; }
+        public string MobileNumber { get; set; }
         public UserStatus Status { get; set; }
         public DateTime CreatedOn { get; set; }
         public DateTime? LastLoginOn { get; set; }
