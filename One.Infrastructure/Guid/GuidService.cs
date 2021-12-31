2a835c98bc18e6d26f987df5bbec5ad88f2d629f
coding-with-nilesh <95519505+coding-with-nilesh@users.noreply.github.com>
2022-01-01 00:29:52 +0530

    Update GuidService.cs

--- One.Infrastructure/Guid/GuidService.cs
+++ One.Infrastructure/Guid/GuidService.cs
@@ -10,7 +10,7 @@ namespace One.Infrastructure.Guid
 
         public string GetShortGuid()
         {
-            var ticks = new DateTime(2021, 1, 1).Ticks; // First data which will be used as reference. Keep it latest to reduce length of id.
+            var ticks = new DateTime(2022, 1, 1).Ticks; // First data which will be used as reference. Keep it latest to reduce length of id.
             var ans = DateTime.Now.Ticks - ticks;
             return ans.ToString("x");
         }
