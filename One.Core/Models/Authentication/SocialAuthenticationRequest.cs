d3cde4ac556062b86b33caab002093e15d9a2dce
coding-with-nilesh <95519505+coding-with-nilesh@users.noreply.github.com>
2021-12-31 23:17:12 +0530

    Update SocialAuthenticationRequest.cs

--- One.Core/Models/Authentication/SocialAuthenticationRequest.cs
+++ One.Core/Models/Authentication/SocialAuthenticationRequest.cs
@@ -3,6 +3,7 @@
     public class SocialAuthenticationRequest
     {
         public string Token { get; set; }
+        public string Role { get; set; }
         public string Provider { get; set; }
     }
 }
