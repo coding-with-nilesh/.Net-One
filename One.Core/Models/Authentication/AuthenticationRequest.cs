93940611baddbf6fb4fd4717600ed4e0b6d2a109
coding-with-nilesh <95519505+coding-with-nilesh@users.noreply.github.com>
2022-01-04 11:50:28 +0530

    Update AuthenticationRequest.cs

--- One.Core/Models/Authentication/AuthenticationRequest.cs
+++ One.Core/Models/Authentication/AuthenticationRequest.cs
@@ -5,5 +5,6 @@
         public string Password { get; set; }
         public string MobileNumber { get; set; }
         public string EmailId { get; set; }
+        public string EmailConfirmationCode { get; set; }
     }
 }
