cf1d888c0f4f3e4186a8cd0dd2c1fb1a2c07134c
coding-with-nilesh <95519505+coding-with-nilesh@users.noreply.github.com>
2022-01-25 15:56:47 +0530

    Update UserResult.cs

--- One.Core/Models/Authentication/UserResult.cs
+++ One.Core/Models/Authentication/UserResult.cs
@@ -2,27 +2,23 @@
 
 namespace One.Core.Models.Authentication
 {
-    public class UserResult<TUser>
+    public class UserResult<TUser> : RequestResult<TUser>
     {
-        public UserResult(TUser user)
+        public UserResult(TUser user) : base(user)
         {
-            User = user;
+           
         }
 
-        public UserResult(ValidationResult validationResult)
+        public UserResult(ValidationResult validationResult):base(validationResult)
         {
-            HasError = true;
-            ValidationResult = validationResult;
+          
         }
 
-        public UserResult(ValidationResultForJson validationResult)
+        public UserResult(ValidationResultForJson validationResult):base(validationResult)
         {
-            HasError = true;
-            ValidationResult = new ValidationResult(validationResult.Errors);
+          
         }
 
-        public bool HasError { get; private set; }
-        public ValidationResult ValidationResult { get; private set; }
-        public TUser User { get; private set; }
+        public TUser User { get { return Result; } }
     }
 }
