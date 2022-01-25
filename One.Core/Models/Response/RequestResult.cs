07e3cbcac8ab4aabce972b322951c524fe76bedc
coding-with-nilesh <95519505+coding-with-nilesh@users.noreply.github.com>
2022-01-25 15:53:19 +0530

    Create RequestResult.cs

---
+++ One.Core/Models/Response/RequestResult.cs
@@ -0,0 +1,28 @@
+﻿using One.Core.Validation;
+
+namespace One.Core.Models.Authentication
+{
+    public class UserResult<TUser>
+    {
+        public UserResult(TUser user)
+        {
+            User = user;
+        }
+
+        public UserResult(ValidationResult validationResult)
+        {
+            HasError = true;
+            ValidationResult = validationResult;
+        }
+
+        public UserResult(ValidationResultForJson validationResult)
+        {
+            HasError = true;
+            ValidationResult = new ValidationResult(validationResult.Errors);
+        }
+
+        public bool HasError { get; private set; }
+        public ValidationResult ValidationResult { get; private set; }
+        public TUser User { get; private set; }
+    }
+}
