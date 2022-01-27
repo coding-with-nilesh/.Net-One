33fb12205b0706a2cba38f804d8d53abf10ffc4f
coding-with-nilesh <95519505+coding-with-nilesh@users.noreply.github.com>
2022-01-27 10:34:34 +0530

    Create ResetPasswordRequest.cs

---
+++ One.Core/Models/Authentication/ResetPasswordRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace One.Core.Models.Authentication
+{
+    public class ResetPasswordRequest
+    {
+        public string EmailId { get; set; }
+        public string Password { get; set; }
+        public string Code { get; set; }
+    }
+}
